7222189e8e925eb47d08ab0cf81dd6a08964e8b2
Francesco <francesco@macFra.local>
2020-08-06 11:36:47 +0200

    assignment 5 version

---
+++ products.js
@@ -0,0 +1,115 @@
+
+window.products = [
+  {
+    type: 'Food',
+    name: 'Mozzarella',
+    description: 'Organic buffalo mozzarella from Campania (250g)',
+    price: '$10.00',
+    image: './images/pinar-kucuk-rq0GMrnLj8c-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Gorgonzola',
+    description: 'Cheese from cows raised on organic feed in Piedmont (250g)',
+    price: '$12.00',
+    image: './images/close-up-photo-of-blue-cheese-4109943.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Gnocchi',
+    description: 'Homemade Gnocchi (24 pcs)',
+    price: '$9.00',
+    image: './images/gabor-molnar-5300Q8dOY18-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Ravioli',
+    description: 'Hamemade ravioli stuffed with ricotta & herbs (24 pcs)',
+    price: '$14.00',
+    image: './images/lindsay-lenard-Qjxfm-OuoMA-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Spaghetti',
+    description: 'Fresh homemade egg pasta (500g)',
+    price: '$10.00',
+    image: './images/wendy-rake-QCidlRqNqkY-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Pesto',
+    price: '$11.00',
+    description: 'Genovese Pesto made with 80% pine nuts from Liguria (250g)',
+    image: './images/milada-vigerova-eqJkqau0C8s-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Olives',
+    price: '$9.00',
+    description: 'Genovese olives from Liguria (500g)',
+    image: './images/olives-on-white-ceramic-bowl-4109918.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Olive Oil',
+    price: '$22.00',
+    description: 'Cold-pressed from Southern Puglia (1L)',
+    image: './images/roberta-sorge-uOBApnN_K7w-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Coffee',
+    price: '$12.00',
+    description: 'Coffee Bag (500g)',
+    image: './images/nadia-valko-7QlJ79Cj43w-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Tomatoes',
+    price: '$10.00',
+    description: 'Seasonal vine tomatoes From Campania (500g)',
+    image: './images/rodion-kutsaev-EPwuZxdketc-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Bread',
+    price: '$2.00',
+    description: 'Freshly baked italian bread',
+    image: './images/jem-sahagun--KLSrKaJhuA-unsplash.jpg'
+  },
+  {
+    type: 'Food',
+    name: 'Spicy Red Pepper',
+    price: '$12.00',
+    description: 'Organic Italian peperoncini from Calabria (375g)',
+    image: './images/john-cameron-BKkeurJTEHo-unsplash.jpg'
+  },
+  {
+    type: 'Drink',
+    name: 'Aranciata',
+    description: 'Cold-pressed juice made with 100% Sicilian oranges (275ml)',
+    price: '$7.00',
+    image: './images/sarah-gualtieri-lAX3fZtTpRk-unsplash.jpg'
+  },
+  {
+    type: 'Drink',
+    name: 'Barolo Wine',
+    price: '$75.00',
+    description: 'Wine made with Nebbiolo grapes from Piedmont region (750ml)',
+    image: './images/roberta-sorge-IywM7AQTZcM-unsplash.jpg'
+  },
+  {
+    type: 'Drink',
+    name: 'White Wine',
+    price: '$15.00',
+    description: 'Arneis Wine from Alba in Piedmont (750ml)',
+    image: './images/celina-albertz-mGhlWTP118Y-unsplash.jpg'
+  },
+  {
+    type: 'Drink',
+    name: 'Beer',
+    price: '$12.00',
+    description: 'Craft Italian beer (300ml)',
+    image: './images/jonas-jacobsson-p2KA3Q2lfSw-unsplash.jpg'
+  }
+];
---
+++ script.js
@@ -0,0 +1,62 @@
+
+// create HTML elements and append content in it
+function setupItems(itemsArray) {
+  itemsArray.forEach(item => {
+    var itemsDiv = document.createElement('div');
+    var itemTitle = document.createElement('h3');
+    var description = document.createElement('p');
+    var price = document.createElement('p');
+    price.className = 'priceTag';
+    var imageElem = document.createElement('img');
+    var addToBag = document.createElement('button');
+    addToBag.className = 'button';
+
+    itemTitle.innerHTML = item.name;
+    description.innerHTML = item.description;
+    price.innerHTML = item.price;
+    imageElem.src = item.image;
+    imageElem.height = '225';
+    addToBag.innerHTML = 'ADD TO BAG';
+
+    itemsDiv.appendChild(itemTitle);
+    itemsDiv.appendChild(description);
+    itemsDiv.appendChild(price);
+    itemsDiv.appendChild(imageElem);
+    itemsDiv.appendChild(addToBag);
+
+    var allItemsDiv = document.querySelectorAll('#allItemsDisplay');
+
+    allItemsDiv.forEach(elem => {
+      elem.appendChild(itemsDiv);
+    });
+  });
+}
+
+function clearTable() {
+  var emptyDiv = document.querySelector('#allItemsDisplay');
+  emptyDiv.innerHTML = '';
+}
+
+// only get Food items into a new array
+var foodArr = window.products.filter(item => item.type === 'Food');
+// only get Drink items into a new array
+var drinkArr = window.products.filter(item => item.type === 'Drink');
+
+// call setupItems function with new array
+function getItemsByType(id, itemsArray, title) {
+  document.querySelector(id).addEventListener('click', function() {
+    var listTitle = document.querySelector('#listTitle');
+    var newTitle = document.createElement('h2');
+    listTitle.innerHTML = '';
+    newTitle.innerHTML = title;
+    listTitle.appendChild(newTitle);
+    clearTable();
+    setupItems(itemsArray);
+  });
+}
+
+getItemsByType('#foodMenu', foodArr, 'Food');
+getItemsByType('#drinkMenu', drinkArr, 'Drinks');
+getItemsByType('#allProductsMenu', window.products, 'All Products');
+
+window.onload = setupItems(window.products);
