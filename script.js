96380535af3c5b7fb49f71d009671a5e1682b5c6
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-04-02 12:56:10 -0400

    cleanup script.js

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-// create HTML elements and append content in it
-function setupItems(itemsArray) {
-  itemsArray.forEach((item) => {
-    var itemsDiv = document.createElement("div");
-    var itemTitle = document.createElement("h3");
-    var description = document.createElement("p");
-    var price = document.createElement("p");
-    price.className = "priceTag";
-    var imageElem = document.createElement("img");
-    var addToBag = document.createElement("button");
-    addToBag.className = "button";
+(function () {
+  // create HTML elements and append content in it
+  const setupItems = (productsArray) => {
+    productsArray.forEach((product) => {
+      const productsDiv = document.createElement("div");
+      const productTitle = document.createElement("h3");
+      const description = document.createElement("p");
+      const price = document.createElement("p");
+      price.className = "priceTag";
+      const imageElem = document.createElement("img");
+      const addToBag = document.createElement("button");
+      addToBag.className = "button";
 
-    itemTitle.innerHTML = item.name;
-    description.innerHTML = item.description;
-    price.innerHTML = new Intl.NumberFormat("en-CA", {
-      style: "currency",
-      currency: "CAD",
-    }).format(item.price);
-    imageElem.src = item.image;
-    imageElem.alt = item.name;
-    imageElem.width = "225";
-    imageElem.height = "225";
-    addToBag.innerHTML = "ADD TO BAG";
+      productTitle.textContent = product.name;
+      description.textContent = product.description;
+      price.textContent = new Intl.NumberFormat("en-CA", {
+        style: "currency",
+        currency: "CAD",
+      }).format(product.price);
+      imageElem.className = "productImage";
+      imageElem.src = product.image;
+      imageElem.alt = product.name;
+      addToBag.textContent = "ADD TO BAG";
 
-    itemsDiv.appendChild(itemTitle);
-    itemsDiv.appendChild(description);
-    itemsDiv.appendChild(price);
-    itemsDiv.appendChild(imageElem);
-    itemsDiv.appendChild(addToBag);
+      productsDiv.appendChild(productTitle);
+      productsDiv.appendChild(description);
+      productsDiv.appendChild(price);
+      productsDiv.appendChild(imageElem);
+      productsDiv.appendChild(addToBag);
 
-    var allItemsDiv = document.querySelectorAll("#allItemsDisplay");
-
-    allItemsDiv.forEach((elem) => {
-      elem.appendChild(itemsDiv);
+      const allItemsDiv = document.querySelector("#allItemsDisplay");
+      allItemsDiv.appendChild(productsDiv);
     });
-  });
-}
+  };
 
-function clearTable() {
-  var emptyDiv = document.querySelector("#allItemsDisplay");
-  emptyDiv.innerHTML = "";
-}
+  const clearTable = () => {
+    const emptyDiv = document.querySelector("#allItemsDisplay");
+    emptyDiv.textContent = "";
+  };
 
-// only get Food items into a new array
-var foodArr = window.products.filter((item) => item.type === window.types.food);
-// only get Drink items into a new array
-var drinkArr = window.products.filter(
-  (item) => item.type === window.types.drink
-);
+  // call setupItems function with new array
+  const getItemsByType = (id, productsArray, title) => {
+    document.querySelector(id).addEventListener("click", () => {
+      const listTitle = document.querySelector("#listTitle");
+      const newTitle = document.createElement("h2");
+      listTitle.textContent = "";
+      newTitle.textContent = title;
+      listTitle.appendChild(newTitle);
+      clearTable();
+      setupItems(productsArray);
+    });
+  };
 
-// call setupItems function with new array
-function getItemsByType(id, itemsArray, title) {
-  document.querySelector(id).addEventListener("click", function () {
-    var listTitle = document.querySelector("#listTitle");
-    var newTitle = document.createElement("h2");
-    listTitle.innerHTML = "";
-    newTitle.innerHTML = title;
-    listTitle.appendChild(newTitle);
-    clearTable();
-    setupItems(itemsArray);
-  });
-}
+  // only get Food products into a new array
+  const foodArr = window.products.filter(
+    (product) => product.type === window.types.food
+  );
 
-getItemsByType("#foodMenu", foodArr, window.types.food);
-getItemsByType("#drinkMenu", drinkArr, window.types.drink);
-getItemsByType("#allProductsMenu", window.products, "All Products");
+  // only get Drink products into a new array
+  const drinkArr = window.products.filter(
+    (product) => product.type === window.types.drink
+  );
 
-window.onload = setupItems(window.products);
+  getItemsByType("#foodMenu", foodArr, window.types.food);
+  getItemsByType("#drinkMenu", drinkArr, window.types.drink);
+  getItemsByType("#allProductsMenu", window.products, "All Products");
+
+  window.addEventListener("load", () => setupItems(window.products));
+})();
